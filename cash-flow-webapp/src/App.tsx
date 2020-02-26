d8e2b0578e7d52d8567931f4d598e7a7dfaab5e9
Paolo Tesser <p.tesser921@gmail.com>
2020-02-26 09:25:37 +0100

    chore(app): add base scaffold

--- cash-flow-webapp/src/App.tsx
+++ cash-flow-webapp/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
     </div>
---
+++ cash-flow-webapp/src/pages/transactions/Transactions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Transactions.css';
+
+const Transactions: React.FC = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Transactions;
